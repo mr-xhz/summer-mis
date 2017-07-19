d65ba4e09101d7d0b34b14f8f12b40ce69ec53b2
张弓 <sz9214e@qq.com>
2017-07-19 14:16:02 +0800

    改进参数值判断

--- src/main/java/cn/cerc/jmis/page/AppLoginPage.java
+++ src/main/java/cn/cerc/jmis/page/AppLoginPage.java
@@ -14,6 +14,7 @@ import cn.cerc.jbean.form.IForm;
 import cn.cerc.jbean.tools.IAppLogin;
 import cn.cerc.jdb.core.IHandle;
 import cn.cerc.jdb.core.Record;
+import cn.cerc.jdb.core.Utils;
 import cn.cerc.jmis.core.RequestData;
 import cn.cerc.jmis.form.AbstractForm;
 
@@ -95,7 +96,7 @@ public class AppLoginPage extends AbstractJspPage implements IAppLogin {
 			}
 		} else {
 			// 登陆验证失败，进行判断，手机号为空，则回到登陆页，手机不为空，密码为空，则跳到发送验证码页面
-			String mobile = app.getDataOut().getHead().getString("Mobile_");
+			String mobile = Utils.safeString(app.getDataOut().getHead().getString("Mobile_"));
 			if (mobile == null || "".equals(mobile)) {
 				log.debug(String.format("用户帐号(%s)与密码认证失败", userCode));
 				req.setAttribute("loginMsg", app.getMessage());
