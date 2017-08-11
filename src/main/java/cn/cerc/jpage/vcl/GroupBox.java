c97a63b67171c564d96d4fa210f00a9bc01c4fcc
韦善贵 <873261248@qq.com>
2017-08-11 14:48:07 +0800

    增加上传控件

--- src/main/java/cn/cerc/jpage/vcl/GroupBox.java
+++ src/main/java/cn/cerc/jpage/vcl/GroupBox.java
@@ -5,6 +5,8 @@ import cn.cerc.jpage.core.HtmlWriter;
 
 public class GroupBox extends Component {
 
+    private String cssClass;
+
     public GroupBox(Component content) {
         super(content);
     }
@@ -14,8 +16,18 @@ public class GroupBox extends Component {
         html.print("<div role='group'");
         if (getId() != null)
             html.print(" id='%s' ", getId());
+        if (this.cssClass != null)
+            html.print(" class='%s' ", cssClass);
         html.print(">");
         super.output(html);
         html.println("</div>");
+    }
+
+    public String getCssClass() {
+        return cssClass;
+    }
+
+    public void setCssClass(String cssClass) {
+        this.cssClass = cssClass;
     }
 }
