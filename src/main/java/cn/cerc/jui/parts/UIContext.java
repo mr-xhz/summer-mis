cd77971a6e7393082c3b0f6add829005ad6094cf
张弓 <sz9214e@qq.com>
2017-11-24 08:57:27 +0800

    增加函数append，用于后续取代appendContext

--- src/main/java/cn/cerc/jui/parts/UIContext.java
+++ src/main/java/cn/cerc/jui/parts/UIContext.java
@@ -1,11 +1,28 @@
 package cn.cerc.jui.parts;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cn.cerc.jpage.core.Component;
+import cn.cerc.jpage.core.HtmlContent;
+import cn.cerc.jpage.core.HtmlWriter;
 
 public class UIContext extends UIComponent {
+    private List<HtmlContent> contents = new ArrayList<>();
 
     public UIContext(Component owner) {
         super(owner);
     }
 
+    public void append(HtmlContent content) {
+        contents.add(content);
+    }
+
+    @Override
+    public void output(HtmlWriter html) {
+        super.output(html);
+        // 输出追加过来的内容
+        for (HtmlContent content : contents)
+            content.output(html);
+    }
 }
