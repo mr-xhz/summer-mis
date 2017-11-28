6c265adc9f2f1c220a8af77aabfba181b9e200fd
黄荣君 <l1091462907@gmail.com>
2017-11-28 11:29:46 +0800

    原来的 UIPageMenu 内部更新为 UIPageSearch，菜单图标显示专用

--- src/main/java/cn/cerc/jui/page/UIPageMenu.java
+++ src/main/java/cn/cerc/jui/page/UIPageMenu.java
@@ -1,81 +1,173 @@
 package cn.cerc.jui.page;
 
+import static cn.cerc.jmis.core.ClientDevice.device_ee;
+
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
+import cn.cerc.jbean.core.Application;
+import cn.cerc.jbean.core.CustomHandle;
 import cn.cerc.jbean.form.IForm;
-import cn.cerc.jmis.core.RequestData;
-import cn.cerc.jui.parts.UIMenuList;
+import cn.cerc.jbean.other.MemoryBuffer;
+import cn.cerc.jbean.rds.PassportRecord;
+import cn.cerc.jdb.core.DataSet;
+import cn.cerc.jmis.form.AbstractForm;
+import cn.cerc.jmis.page.AbstractJspPage;
+import cn.cerc.jmis.page.ExportFile;
+import cn.cerc.jmis.page.IMenuBar;
+import cn.cerc.jpage.core.Component;
+import cn.cerc.jpage.core.HtmlContent;
+import cn.cerc.jpage.core.UrlRecord;
+import cn.cerc.jpage.grid.AbstractGrid;
+import cn.cerc.jpage.grid.GridFactory;
+import cn.cerc.jpage.grid.MutiPage;
+import cn.cerc.jpage.other.OperaPages;
+import cn.cerc.jui.parts.MainMenu;
+import cn.cerc.jui.parts.RightMenus;
+import cn.cerc.jui.parts.UIComponent;
+import cn.cerc.jui.parts.UIFormHorizontal;
+import cn.cerc.jui.parts.UIFormVertical;
 
 /**
- * 系统登录页
+ * 主体子页面
+ * 
+ * @author 张弓
+ *
  */
-public class UIPageMenu extends UIPageDialog {
-    private UIMenuList menus;
+public class UIPageMenu extends AbstractJspPage {
+    private MutiPage pages;
+    private String searchWaitingId = "";
 
     public UIPageMenu(IForm form) {
         super(form);
+        this.addCssFile("css/summer.css");
+        if (!form.getClient().isPhone()) {
+            this.addCssFile("css/summer-pc.css");
+        }
+        this.addScriptFile("js/jquery.js");
+        this.addScriptFile("js/summer.js");
+        this.addScriptFile("js/myapp.js");
+    }
+
+    public void addExportFile(String service, String key) {
+        if (device_ee.equals(this.getForm().getClient().getDevice())) {
+            ExportFile item = new ExportFile(service, key);
+            this.put("export", item);
+        }
     }
 
     @Override
     public void execute() throws ServletException, IOException {
-        if (menus != null)
-            add("menus", menus);
-        super.execute();
+        HttpServletRequest request = getRequest();
+        MainMenu mainMenu = getMainMenu();
+
+        // 添加分页控制
+        Component operaPages = null;
+        if (pages != null) {
+            this.put("pages", pages);
+            operaPages = new OperaPages(this.getForm(), pages);
+            this.put("_operaPages_", operaPages);
+        }
+        IForm form = this.getForm();
+        CustomHandle sess = (CustomHandle) form.getHandle().getProperty(null);
+        if (sess.logon()) {
+            List<UrlRecord> rightMenus = mainMenu.getRightMenus();
+            RightMenus menus = Application.getBean("RightMenus", RightMenus.class);
+            menus.setHandle(form.getHandle());
+            for (IMenuBar item : menus.getItems())
+                item.enrollMenu(form, rightMenus);
+        } else {
+            mainMenu.getHomePage().setSite(Application.getAppConfig().getFormWelcome());
+        }
+
+        // 系统通知消息
+        Component content = this.getContent();
+        if (form instanceof AbstractForm) {
+            this.getHeader().initHeader();
+            request.setAttribute(content.getId(), content);
+            for (Component component : content.getComponents()) {
+                request.setAttribute(component.getId(), component);
+            }
+        }
+
+        // 开始输出
+        PrintWriter out = getResponse().getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.printf("<title>%s</title>\n", this.getForm().getTitle());
+        out.printf("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/>\n");
+        out.printf(
+                "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0\"/>\n");
+        out.print(this.getCssHtml());
+        out.print(getScriptHtml());
+        out.println("<script>");
+        out.println("var Application = new TApplication();");
+        out.printf("Application.device = '%s';\n", form.getClient().getDevice());
+
+        out.printf("Application.bottom = '%s';\n", this.getFooter().getId());
+
+        String msg = form.getParam("message", "");
+        msg = msg == null ? "" : msg.replaceAll("\r\n", "<br/>");
+        out.printf("Application.message = '%s';\n", msg.replace("'", "\\'"));
+        out.printf("Application.searchFormId = '%s';\n", this.searchWaitingId);
+        out.println("$(document).ready(function() {");
+        out.println("Application.init();");
+        out.println("});");
+        out.println("</script>");
+        out.println("</head>");
+        outBody(out);
+        out.println("</html>");
     }
 
-    public UIMenuList getMenus() {
-        if (menus == null)
-            menus = new UIMenuList(this.getDocument().getContent());
-        return menus;
+    public void appendContent(HtmlContent content) {
+        this.getDocument().getContent().append(content);
     }
 
-    public void setDisableAccountSave(boolean value) {
-        super.put("disableAccountSave", value);
+    public UIFormHorizontal createSearch(MemoryBuffer buff) {
+        UIFormHorizontal search = new UIFormHorizontal(this.getDocument().getContent(), this.getRequest());
+        search.setBuffer(buff);
+        this.setSearchWaitingId(search.getId());
+        return search;
     }
 
-    public void setDisablePasswordSave(boolean value) {
-        super.put("DisablePasswordSave", value);
+    public AbstractGrid createGrid(UIComponent owner, DataSet dataSet) {
+        AbstractGrid grid = GridFactory.build(this.getForm(), owner);
+        grid.setDataSet(dataSet);
+        pages = grid.getPages();
+        return grid;
     }
 
-    public void setStartVine(boolean value) {
-        super.put("startVine", value);
+    public String getSearchWaitingId() {
+        return searchWaitingId;
     }
 
-    public void setStartHost(String value) {
-        super.add("startHost", value);
+    public void setSearchWaitingId(String searchWaitingId) {
+        this.searchWaitingId = searchWaitingId;
     }
 
-    public void setSessionKey(Object value) {
-        super.put(RequestData.appSession_Key, value);
+    public void add(String id, AbstractGrid grid) {
+        getRequest().setAttribute(id, grid);
+        pages = grid.getPages();
     }
 
-    public void setMenus(Object value) {
-        super.put("menus", value);
+    public void add(String id, UIFormHorizontal value) {
+        put(id, value);
     }
 
-    public void setOnlineUsers(int value) {
-        super.put("onlineUsers", value);
+    public void add(String id, UIFormVertical value) {
+        put(id, value);
     }
 
-    public void setCurrentUserCode(String value) {
-        super.add("currentUserCode", value);
+    public void add(String id, ExportFile value) {
+        put(id, value);
     }
 
-    public void setCurrentUserName(String value) {
-        super.add("currentUserName", value);
-    }
-
-    public void setCurrentCorpName(String value) {
-        super.add("currentCorpName", value);
-    }
-
-    public void setIsViewOldMenu(boolean value) {
-        super.put("isViewOldMenu", value);
-    }
-
-    public void setUnReadMessage(int value) {
-        super.put("unReadMessage", value);
+    public void add(String id, PassportRecord value) {
+        put(id, value);
     }
 }
