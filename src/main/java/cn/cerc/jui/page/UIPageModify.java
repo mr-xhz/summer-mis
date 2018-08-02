dbba157166f4baa088c7017c046044d260bc828e
黄荣君 <l1091462907@gmail.com>
2018-08-02 12:01:06 +0800

    自动生成的所有的请求都不发送 referrer，增加防盗链

--- src/main/java/cn/cerc/jui/page/UIPageModify.java
+++ src/main/java/cn/cerc/jui/page/UIPageModify.java
@@ -79,6 +79,10 @@ public class UIPageModify extends AbstractJspPage {
         out.println("<html>");
         out.println("<head>");
         out.printf("<title>%s</title>\n", this.getForm().getTitle());
+
+        // 所有的请求都不发送 referrer
+        out.println("<meta name=\"referrer\" content=\"no-referrer\" />");
+
         out.printf("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/>\n");
         out.println("<meta http-equiv=\"X-UA-Compatible\" content=\"IE=9; IE=8; IE=7;\"/>");
         out.printf(
